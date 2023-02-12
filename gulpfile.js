9de87a3a70deba21fa81999bac6faaf81acb31f9
wrld <eelwrld@gmail.com>
2023-02-12 22:57:00 +0500

    first screen

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,10 @@ gulp.task('styles', function(){
 gulp.task('watch', function(){
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"))
     gulp.watch("src/*.html").on("change", gulp.parallel('html'));
+    gulp.watch("src/js/**/*.js").on("change", gulp.parallel('scripts'))
+    gulp.watch("src/fonts/**/*").on("all", gulp.parallel('fonts'))
+    gulp.watch("src/img/**/*").on("add", gulp.parallel('images'))
+    gulp.watch("src/icons/**/*").on("add", gulp.parallel('icons'))
 });
 
 gulp.task('html', function(){
@@ -45,24 +49,29 @@ gulp.task('html', function(){
 
 gulp.task('scripts', function(){
     return gulp.src("src/js/**/*.js")
-    .pipe(gulp.dest("dist/js"));
+        .pipe(gulp.dest("dist/js"))
+        .pipe(browserSync.stream());
 })
 gulp.task('fonts', function(){
     return gulp.src("src/fonts/**/*")
-    .pipe(gulp.dest("dist/fonts"));
+    .pipe(gulp.dest("dist/fonts"))
+    .pipe(browserSync.stream());
 })
 gulp.task('icons', function(){
     return gulp.src("src/icons/**/*")
-    .pipe(gulp.dest("dist/icons"));
+    .pipe(gulp.dest("dist/icons"))
+    .pipe(browserSync.stream());
 })
 gulp.task('mailer', function(){
     return gulp.src("src/mailer/**/*")
-    .pipe(gulp.dest("dist/mailer"));
+    .pipe(gulp.dest("dist/mailer"))
+    .pipe(browserSync.stream());
 })
 gulp.task('images', function(){
     return gulp.src("src/img/**/*")
     .pipe(imagemin())
-    .pipe(gulp.dest("dist/img"));
+    .pipe(gulp.dest("dist/img"))
+    .pipe(browserSync.stream());
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'scripts', 'fonts', 'icons', 'mailer', 'images'));
