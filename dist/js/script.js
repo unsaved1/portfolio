421c3f62c835e6f0b6c16e0e6881ac23c35eed16
wrld <eelwrld@gmail.com>
2023-02-14 01:12:17 +0500

    third section

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,11 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 })
+
+const numb = document.querySelectorAll('.skills__dial-numb'),
+      lines = document.querySelectorAll('.skills__dial-level .skills__dial-line');
+
+numb.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+    
+})
