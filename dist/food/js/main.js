b596dc294d1f5038129e79ecf36b46a9324d1725
wrld <eelwrld@gmail.com>
2023-03-22 23:17:30 +0500

    add js project

--- dist/food/js/main.js
+++ dist/food/js/main.js
@@ -221,87 +221,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //FORMS
 
-    // const forms = document.querySelectorAll('form');
-
-    // const message = {
-    //     loading: 'img/form/spinner.svg',
-    //     success: 'Спасибо, мы скоро с вами свяжемся',
-    //     failure: 'Что-то пошло не так...'
-    // };
-
-    // forms.forEach(item => {
-    //     postData(item);
-    // })
-
-    // function postData(form) {
-    //     form.addEventListener('submit', (e) => {
-    //         e.preventDefault();
-
-    //         const statusMessage = document.createElement('img');
-    //         statusMessage.src = message.loading;
-    //         statusMessage.style.cssText = `
-    //             display: block;
-    //             margin: 0 auto;
-    //         `;
-    //         form.insertAdjacentElement('afterend', statusMessage);
-
-    //         const req = new XMLHttpRequest();
-    //         req.open('POST', 'server.php');
-            
-    //         req.setRequestHeader('Content-type', 'applicaation/json');
-    //         const formData = new FormData(form);
-
-    //         const obj = {};
-    //         formData.forEach(function(value, key) {
-    //             obj[key] = value;
-    //         })
-
-    //         const json = JSON.stringify(obj);
-    //         req.send(json);
-
-    //         req.addEventListener('load', () => {
-    //             if (req.status === 200) {
-    //                 console.log(req.response);
-    //                 showThanksModal(message.success);
-    //                 form.reset();
-    //                 statusMessage.remove();
-    //             } else {
-    //                 showThanksModal(message.failure);
-    //             }
-    //         })
-    //     })
-    // }
-
-
-    // function showThanksModal(message) {
-    //     const prevModalDialog = document.querySelector('.modal__dialog');
-
-    //     prevModalDialog.classList.add('hide');
-    //     modalOpen();
-
-    //     const thanksModal = document.createElement('div');
-    //     thanksModal.classList.add('modal__dialog');
-    //     thanksModal.innerHTML = `
-    //     <div class = 'modal__content'>
-    //         <div class='modal__close' data-close>&times;</div>
-    //         <div class='modal__title'>${message}</div>
-    //     </div>
-    //     `;
-
-    //     document.querySelector('.modal').append(thanksModal);
-
-    //     setTimeout(() => {
-    //         thanksModal.remove();
-    //         prevModalDialog.classList.add('show');
-    //         prevModalDialog.classList.remove('hide');
-    //         modalClose();
-    //     }, 1000)
-
-        
-    // }
-
-
-
     const forms = document.querySelectorAll('form');
 
     const message = {
@@ -391,65 +310,120 @@ document.addEventListener('DOMContentLoaded', () => {
     //Slider
 
     const slides = document.querySelectorAll('.offer__slide'),
+          slider = document.querySelector('.offer__slider'),
           prevBtn = document.querySelector('.offer__slider-prev'),
           nextBtn = document.querySelector('.offer__slider-next'),
-          indexCounter = document.querySelector('#current');
-    let id = 3;
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current'),
+          slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+          slidesField = document.querySelector('.offer__slider-inner'),
+          width = window.getComputedStyle(slidesWrapper).width;
+    
+    let slideIndex = 1;
+    let offset = 0;
 
-    function showSlide(n) {
-        slides[n-1].classList.add('show');
-        slides[n-1].classList.remove('hide');
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
+    } else {
+        total.textContent = slides.length;
+        current.textContent = slideIndex;
     }
 
-    function hideSlide() {
-        slides.forEach(slide => {
-            slide.classList.add('hide');
-            slide.classList.remove('show');
-        })
-    } 
-
-
-    function swipeSlide(id) {
-        hideSlide();
-        showSlide(id);
-        slides[id-1].classList.add('fade');
-    }
-
-    function changeIndexCounter(id) {
-        if (id < 10) {
-            indexCounter.textContent = `0${id}`;    
-        } else {
-            indexCounter.textContent = `${id}`;
-        }
-        
-    }
-
-
-    hideSlide();
-    showSlide(id);
-
-    nextBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        if (id >= 4) {
-            id = 0;
-        }
-        id += 1;
-        swipeSlide(id);
-        changeIndexCounter(id);
-    })
-
-    prevBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        if (id <= 1) {
-            id = 5;
-        }
-        id -= 1;
-        swipeSlide(id);
-        changeIndexCounter(id);
-    })
 
     
 
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+         slide.style.width = width;
+    });
+
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+          dots = [];
+    indicators.classList.add('carousel-indicators');
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.classList.add('dot');
+
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+
+    nextBtn.addEventListener('click', () => {
+        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        } 
+
+        setSlideIndex();
+    });
+
+    prevBtn.addEventListener('click', () => {
+        if (offset == 0){
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex =  slides.length;
+        } else {
+            slideIndex--;
+        } 
+
+        setSlideIndex();
+        setActiveDot();
+    });
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (event) => {
+            const slideTo = event.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            setSlideIndex();
+            setActiveDot();
+        })
+    })
+
+    function setSlideIndex() {
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    }
+
+    function setActiveDot() {
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
+    }
 });
 
 
