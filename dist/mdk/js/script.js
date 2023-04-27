a296831de5bbe0457567b2c78c12b53f12a6f3db
wrld <eelwrld@gmail.com>
2023-04-28 02:34:43 +0500

    add modal windows

--- dist/mdk/js/script.js
+++ dist/mdk/js/script.js
@@ -136,3 +136,73 @@ function showTabContent(i = 0) {
 
 hideTabContent();
 showTabContent();
+
+
+// modal
+
+
+const modal = document.querySelector('.modal'),
+      loginWindow = document.querySelector('[data-login-window]'),
+      registerWindow = document.querySelector('[data-register-window]'),
+      btnLoginOpen = document.querySelector('[data-profile-login]'), 
+      btnsRegisterOpen = document.querySelectorAll('[data-register]'), 
+      btnsClose = document.querySelectorAll('[data-modal-close]');
+
+
+btnLoginOpen.addEventListener('click', (e) => {
+    e.preventDefault();
+    closeModal(registerWindow, 'modal__window_active');
+    openModal();
+    openModal(loginWindow, 'modal__window_active');
+});
+
+btnsRegisterOpen.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        closeModal(loginWindow, 'modal__window_active');
+        openModal(registerWindow, 'modal__window_active');
+        if (!modal.classList.contains('modal_active')) {
+            openModal();
+            openModal(registerWindow, 'modal__window_active');
+        } 
+    })
+})
+
+
+btnsClose.forEach(btn => {
+    btn.addEventListener('click', () => {
+        closeModal();
+    });
+})
+
+
+
+modal.addEventListener('click', (e) => {
+    if (e.target.classList.contains('modal')) {
+        closeModal();
+    }
+})
+
+modal.addEventListener('keydown', (e) => {
+    if (e.code == 'Escape' && modal.classList.contains('modal_active')) {
+        closeModal();
+    }
+})
+
+
+
+
+function openModal(window = modal, classActive = 'modal_active') {
+    // modal.classList.add('modal_active');
+    window.classList.add(`${classActive}`);
+    document.querySelector('body').style.overflowY = 'hidden';
+}
+
+function closeModal(window = modal, classActive = 'modal_active') {
+    window.classList.remove(`${classActive}`);
+    document.querySelector('body').style.overflowY = 'scroll';
+}
+
+
+    
+
